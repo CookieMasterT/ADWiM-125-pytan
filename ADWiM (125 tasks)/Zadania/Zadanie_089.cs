eb48b5391d8aba45ca35ab1e9727ea9f71ce9701
tytus <tytus@baranscy.info>
2026-02-21 14:15:03 +0100

    Update task 89

--- ADWiM (125 tasks)/Zadania/Zadanie_089.cs
+++ ADWiM (125 tasks)/Zadania/Zadanie_089.cs
@@ -8,8 +8,19 @@ namespace ADWiM__125_tasks_.Zadania
     {
         public int ID { get; set; } = 89;
         public string Text { get; set; } = "89. Opisz znaczenie i potencjalne zastosowania delegaty EventHandler.  ";
+
+        public event EventHandler MyEvent;
+
         public void Execute()
         {
+            var A = (int x, object sender) => Console.WriteLine($"Delegate A with x = '{x}' Invoked by {sender}");
+            Action<int> B = (x) => Console.WriteLine($"Delegate B x = '{x}'");
+
+            MyEvent += (sender, e) => A(2, sender);
+            MyEvent += (sender, e) => B(3);
+
+            MyEvent?.Invoke(this, EventArgs.Empty);
+
             Console.WriteLine(@"EventHandler to standardowy delegat (jak Func, Action, Predicate),
 zaprojektowany do obsługi zdarzeń (przeznaczony jest do przekazania w Event.)
 
